26d31f655545228b562c11caa62334063d538694
Bhaswati Das <76488410+BhaswatiDas1806@users.noreply.github.com>
2021-07-21 13:18:14 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function bottel(){
+    window.location="a.html";
+}
+
+function almirah(){
+    window.location="d.html";
+}
+
+function chair(){
+    window.location="c.html";
+}
+
+function person(){
+    window.location="b.html";
+}
